24d5a68229da4af027f2135cb4db52913a58be99
hassanfalh50 <89648962+hassanfalh50@users.noreply.github.com>
2021-10-28 19:46:18 +0300

    Update par.js

--- par.js
+++ par.js
@@ -1,68 +1,13 @@
-$(document).ready(function() {
-
-  $('#example-1').progress_fnc();
-  $('#example-2').progress_fnc();
-  $('#example-3').progress_fnc();
-  $('#example-4').progress_fnc();
-  $('#example-5').progress_fnc();
-  $('#example-6').progress_fnc();
-  $('#example-7').progress_fnc();
-  $('#example-8').progress_fnc();
-  $('#example-9').progress_fnc();
-  $('#example-10').progress_fnc();
-  $('#example-11').progress_fnc();
-  $('#example-12').progress_fnc();
-  $('#example-13').progress_fnc();
-
-  $('.progressStart').on('click', function() {
-    var perent = $(this).closest("div").attr("id");
-    $('#' + perent).progress_fnc({ type: 'start' });
+$('.bar-percentage[data-percentage]').each(function () {
+  var progress = $(this);
+  var percentage = Math.ceil($(this).attr('data-percentage'));
+  $({countNum: 0}).animate({countNum: percentage}, {
+    duration: 2000,
+    easing:'linear',
+    step: function() {
+      // What todo on every count
+      var pct = Math.floor(this.countNum) + '%';
+      progress.text(pct) && progress.siblings().children().css('width',pct);
+    }
   });
-
-  $('.progressReset').on('click', function() {
-    var perent = $(this).closest("div").attr("id");
-    $('#' + perent).progress_fnc({ type: 'reset' });
-  });
-
 });
-
-
-(function($) {
-
-  $.fn.progress_fnc = function(options) {
-    var settings = $.extend({
-      type: 'start'
-    }, options);
-
-    var div = $(this);
-    var progress = div.find('.cssProgress');
-
-    progress.each(function() {
-      var self = $(this);
-      var progress_bar = self.find('.cssProgress-bar');
-      var progress_label = self.find('.cssProgress-label, .cssProgress-label2');
-      var progress_value = progress_bar.data('percent');
-      var percentage = parseInt(progress_value, 10) + '%';
-
-      progress_bar.css({'width': '0%', 'transition': 'none', '-webkit-transition': 'none', '-moz-transition': 'none'});
-
-      if(settings.type == 'start') {
-
-        progress_bar.animate({
-          width: percentage
-        }, {
-          duration: 1000,
-          step: function(x) {
-            progress_label.text(Math.round(x) + '%');
-          }
-        });
-
-      } else if(settings.type == 'reset') {
-        progress_bar.css('width', '0%');
-        progress_label.text('0%');
-      }
-
-    });
-  }
-
-}(jQuery));
