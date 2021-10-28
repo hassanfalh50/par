01382f83151841191c47ad6795ff4a7efd7443a9
hassanfalh50 <89648962+hassanfalh50@users.noreply.github.com>
2021-10-28 20:30:33 +0300

    Update par.js

--- par.js
+++ par.js
@@ -1,12 +1,42 @@
-let text = document.querySelector(".num");
+var el = document.getElementById('graph'); // get canvas
 
-let numToText = 1;
+var options = {
+  percent: el.getAttribute('data-percent') || 0,
+  size: el.getAttribute('data-size') || 89,
+  lineWidth: el.getAttribute('data-line') || 10,
+  rotate: el.getAttribute('data-rotate') || 0
+}
 
-let time = setInterval(function(){
-    numToText += 1;
-    text.textContent = numToText + "%";
+var canvas = document.createElement('canvas');
+var span = document.createElement('span');
+span.classList.add('canvas-text');
+span.textContent = options.percent + '%';
 
-    if(numToText === 70){
-        clearInterval(time);
-    }
-},14)
+if (typeof(G_vmlCanvasManager) !== 'undefined') {
+  G_vmlCanvasManager.initElement(canvas);
+}
+
+var ctx = canvas.getContext('2d');
+canvas.width = canvas.height = options.size;
+
+el.appendChild(span);
+el.appendChild(canvas);
+
+ctx.translate(options.size / 2, options.size / 2); // change center
+ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI); // rotate -90 deg
+
+//imd = ctx.getImageData(0, 0, 240, 240);
+var radius = (options.size - options.lineWidth) / 2;
+
+var drawCircle = function(color, lineWidth, percent) {
+  percent = Math.min(Math.max(0, percent || 1), 1);
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
+  ctx.strokeStyle = color;
+  ctx.lineCap = 'round'; // butt, round or square
+  ctx.lineWidth = lineWidth
+  ctx.stroke();
+};
+
+drawCircle('#efefef', options.lineWidth, 100 / 100);
+drawCircle('#00c874', options.lineWidth, options.percent / 100);
